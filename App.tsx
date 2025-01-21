4151ee6e455147d5c5a92d962840a34c44687c58
karan <karanc4143@gmail.com>
2025-01-21 22:54:22 +0530

    app is runing

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export class App extends Component {
+  render() {
+    return (
+      <View style={{ backgroundColor: 'black', flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text style={{ color: 'white', fontSize: 24 }}>karan</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
---
+++ src/quizData.tsx
@@ -0,0 +1,35 @@
+const quizData = [
+    {
+      question: "What is the time complexity of binary search?",
+      options: ["O(n)", "O(log n)", "O(n^2)", "O(1)"],
+      correctAnswer: "O(log n)",
+      correctAnswerNum: 1, // Index of the correct answer in the options array
+    },
+    {
+      question: "Which of the following is a JavaScript framework?",
+      options: ["React", "Angular", "Vue", "All of the above"],
+      correctAnswer: "All of the above",
+      correctAnswerNum: 3, // Index of the correct answer in the options array
+    },
+    {
+      question: "What does CSS stand for?",
+      options: ["Cascading Style Sheets", "Cascading Simple Sheets", "Creative Style Sheets", "Computer Style Sheets"],
+      correctAnswer: "Cascading Style Sheets",
+      correctAnswerNum: 0, // Index of the correct answer in the options array
+    },
+    {
+      question: "Which of these is NOT a valid data type in JavaScript?",
+      options: ["String", "Boolean", "Integer", "Object"],
+      correctAnswer: "Integer",
+      correctAnswerNum: 2, // Index of the correct answer in the options array
+    },
+    {
+      question: "What is the output of 2 + '2' in JavaScript?",
+      options: ["4", "22", "NaN", "Error"],
+      correctAnswer: "22",
+      correctAnswerNum: 1, // Index of the correct answer in the options array
+    },
+  ];
+  
+  export default quizData;
+  
